a73c7b78872e5f349d1ba4dac42ed6bbd7f4fb3b
Sebastien Leroch <sebastien.leroch@outlook.fr>
2020-10-24 13:37:48 +0200

    Embellir son bonjour

--- b000000000-structure.ps1
+++ b000000000-structure.ps1
@@ -6,9 +6,12 @@
     Ce script est utilisé pour le laboratoire de programmation en Powershell.
 
 .NOTES
-    Author: CollegeBoreal
-    Derniere mise à jour: yyyy-mm-dd
+    Author: Sebastien Leroch
+    Derniere mise à jour: 2020-10-24 
 
 #>
 
-Write-Host "Bonjour Monde"
+$personneNom = "Alice"
+$personneAge = 35
+
+Write-Host "Bonjour $personneNom, tu as $personneAge ans. "
